3257a803c6f1f54946a8f4462abc25cbbad03148
Daniel_Gómez_G <dgomez.informatica@gmail.com>
2021-03-18 20:32:04 -0300

    Clase con problemas

--- src/ModeloComponente.java
+++ src/ModeloComponente.java
@@ -1,18 +1,18 @@
 
-import javax.swing.table.DefaultTableModel;
-import javax.swing.table.TableModel;
+//import javax.swing.table.DefaultTableModel;
+//import javax.swing.table.TableModel;
 
 /**
  * @author dgomez
  */
 public class ModeloComponente {
 
-    DefaultTableModel modeloTablaContacto;
-
-    public TableModel columnasTablaContacto() {
-        Object[] columnas = new Object[]{"DNI", "NOMBRE", "APELLIDOS", "CELULAR", "CORREO"};
-        modeloTablaContacto = new DefaultTableModel(columnas, 0);
-
-        return modeloTablaContacto;
-    }
+//    DefaultTableModel modeloTablaContacto;
+//
+//    public TableModel columnasTablaContacto() {
+//        Object[] columnas = new Object[]{"DNI", "NOMBRE", "APELLIDOS", "CELULAR", "CORREO"};
+//        modeloTablaContacto = new DefaultTableModel(columnas, 0);
+//
+//        return modeloTablaContacto;
+//    }
 }
